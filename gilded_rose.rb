03983f4ec58d5519db2a60130e75ead19d1a7a81
Jasmine Frantz <hyunfrantz@gmail.com>
2023-09-28 15:16:08 -0400

    initial commit

---
+++ gilded_rose.rb
@@ -0,0 +1,73 @@
+class GildedRose
+
+  def initialize(items)
+    @items = items
+  end
+
+  AGED_BRIE = "Aged Brie"
+  BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+  SULFURAS = "Sulfuras, Hand of Ragnaros"
+
+
+  def update_quality()
+    @items.each do |item|
+      if item.name != AGED_BRIE and item.name != BACKSTAGE_PASS
+        if item.quality > 0
+          if item.name != SULFURAS
+            item.quality = item.quality - 1
+          end
+        end
+      else
+        if item.quality < 50
+          item.quality = item.quality + 1
+          if item.name == BACKSTAGE_PASS
+            if item.sell_in < 11
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+            end
+            if item.sell_in < 6
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+            end
+          end
+        end
+      end
+      if item.name != SULFURAS
+        item.sell_in = item.sell_in - 1
+      end
+      if item.sell_in < 0
+        if item.name != AGED_BRIE
+          if item.name != BACKSTAGE_PASS
+            if item.quality > 0
+              if item.name != SULFURAS
+                item.quality = item.quality - 1
+              end
+            end
+          else
+            item.quality = item.quality - item.quality
+          end
+        else
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+      end
+    end
+  end
+end
+
+class Item
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def to_s()
+    "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
