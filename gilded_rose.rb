c969083da2db2049ea61446fe43e942b11416858
Jasmine Frantz <hyunfrantz@gmail.com>
2023-10-01 13:26:43 -0400

    make test pass, add test

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -4,23 +4,19 @@ class GildedRose
     @items = items
   end
 
-  AGED_BRIE = "Aged Brie"
-  BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
-  SULFURAS = "Sulfuras, Hand of Ragnaros"
-
 
   def update_quality()
     @items.each do |item|
-      if item.name != AGED_BRIE and item.name != BACKSTAGE_PASS
+      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
-          if item.name != SULFURAS
+          if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
           end
         end
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == BACKSTAGE_PASS
+          if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -34,14 +30,14 @@ class GildedRose
           end
         end
       end
-      if item.name != SULFURAS
+      if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
       if item.sell_in < 0
-        if item.name != AGED_BRIE
-          if item.name != BACKSTAGE_PASS
+        if item.name != "Aged Brie"
+          if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-              if item.name != SULFURAS
+              if item.name != "Sulfuras, Hand of Ragnaros"
                 item.quality = item.quality - 1
               end
             end
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,13 +1,60 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
+require 'date'
 
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+    context "item" do
+      it "does not change the name" do
+        items = [Item.new("foo", 0, 0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].name).to eq "foo"
+      end
     end
+
+   # - "Aged Brie" actually increases in Quality the older it gets
+    context "if the item is 'Aged Brie' " do
+      it "does not decrease quality when sell by date has passed" do
+        items = [Item.new("Aged Brie", 10, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 11
+      end
+    end
+
+    # - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality 
+    #(not true, test doesn't prove it)
+    context "if the item is 'Sulfuras'" do
+      it "does not decrease quality when sell by date has passed" do
+        items = [Item.new("Sulfuras", 10, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 10
+      end
+    end
+
+    # - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+    # Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+    # Quality drops to 0 after the concert
+    context "if the item is 'Backstage passes'" do
+      it "increases quality as it gets closer to concert date" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 13
+      end
+
+      it "drops quality to 0 after it has past concert date" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+      end
+    end
+
+
+    # - The Quality of an item is never negative
+    # - Once the sell by date has passed, Quality degrades twice as fast
+    # - The Quality of an item is never more than 50-----
+
+  
+
   end
 
 end
