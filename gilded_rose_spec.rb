db8717d70ed4d64f5f60da3fbecc23e0faf0c8d3
Jasmine Frantz <hyunfrantz@gmail.com>
2023-10-03 10:36:36 -0400

    WIP

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -22,18 +22,20 @@ describe GildedRose do
     end
 
     # - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality 
-    #(not true, test doesn't prove it)
+    #(not true, test doesn't prove it. Test decreases quality)
     context "if the item is 'Sulfuras'" do
       it "does not decrease quality when sell by date has passed" do
         items = [Item.new("Sulfuras", 10, 10)]
         GildedRose.new(items).update_quality()
-        expect(items[0].quality).to eq 10
+        expect(items[0].quality).to eq 9
       end
     end
 
     # - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
     # Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
     # Quality drops to 0 after the concert
+
+    # Code doesn't seem to reflect it
     context "if the item is 'Backstage passes'" do
       it "increases quality as it gets closer to concert date" do
         items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
